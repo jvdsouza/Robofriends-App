dad406d79edc8a0cd089fbc5be368695cccac8a4
Jason D'souza <jvincentdsouza@gmail.com>
2019-01-29 14:34:03 +1100

    Updates

---
+++ precache-manifest.1f0fcc15120ff61f425e3fce8ab10c7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robofriends-App/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1b26cdd16887e8d8b498",
+    "url": "/Robofriends-App/static/js/runtime~main.1b26cdd1.js"
+  },
+  {
+    "revision": "f1b3a3e7a3b4b5d0ca28",
+    "url": "/Robofriends-App/static/js/main.f1b3a3e7.chunk.js"
+  },
+  {
+    "revision": "0c17b7f7a060eea4fbd6",
+    "url": "/Robofriends-App/static/js/1.0c17b7f7.chunk.js"
+  },
+  {
+    "revision": "f1b3a3e7a3b4b5d0ca28",
+    "url": "/Robofriends-App/static/css/main.a1a00007.chunk.css"
+  },
+  {
+    "revision": "0c17b7f7a060eea4fbd6",
+    "url": "/Robofriends-App/static/css/1.72932453.chunk.css"
+  },
+  {
+    "revision": "8b28fa011b81417d4d35a4917ce6a19b",
+    "url": "/Robofriends-App/index.html"
+  }
+];
