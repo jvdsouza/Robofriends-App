1c6ca52d294409dd2c377144f0e95e02af622e81
Jason D'souza <jvincentdsouza@gmail.com>
2019-01-29 20:27:15 +1100

    completed the lighthouse testing with a score fo 99 (99 in performance, 100 in every other category)

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,7 +7,7 @@ const Card = ({name, email, id}) => { //not destructuring inside the input means
   //const {name, email, id} = robotprop; //so we dont have to use robotprop.id or robotprop.name or robotprop.email
   return (
     <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-      <img alt='robots' src={`https://robohash.org/${id}?200x200`}/>
+      <img alt='robots' src={`https://robohash.org/${id}?size=200x200`}/>
       <div>
         <h2>{name}</h2> {/*have to use robotprop.name from the input of the class if we dont make the const at the top */}
         <p>{email}</p> {/*have to use robotprop.email from the input of the class if we dont make the const at the top */}
