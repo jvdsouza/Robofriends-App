437764f037c13ca4b3c0f05226d1746934de04d6
Jason D'souza <jvincentdsouza@gmail.com>
2019-01-31 22:57:12 +1100

    added an api call function

--- src/actions.js
+++ src/actions.js
@@ -1,3 +1,4 @@
+import apiCall from './api/api';
 import {
   CHANGE_SEARCH_FIELD,
   REQUEST_ROBOTS_PENDING,
@@ -12,10 +13,7 @@ export const setSearchField = (text) => ({
 
 export const requestRobots = () => dispatch => {
   dispatch({ type: REQUEST_ROBOTS_PENDING })
-  fetch('https://jsonplaceholder.typicode.com/users') //obtain json data
-    .then(response => {
-       return response.json();
-    })
+  apiCall('https://jsonplaceholder.typicode.com/users') //obtain json data
     .then(data => dispatch({ type:REQUEST_ROBOTS_SUCCESS, payload: data }))
     .catch(error => dispatch({ type: REQUEST_ROBOTS_FAILED, payload: error }))
 }
